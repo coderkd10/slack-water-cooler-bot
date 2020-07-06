856e8693f37e22e589790fd6588f3cd2525653e2
Abhishek Kedia <abhishek.kedia@getblueshift.com>
2020-07-06 15:42:04 +0530

    add logging to entry point function

--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 
 # should be added to a cronjob firing everyday
 # e.g at 10 AM IST everyday
-# 30 4 * * * python3 path/main.py >> logfile
+# 30 4 * * * python3 path/main.py >> path/cronjob.log 2>&1
 
 import time
 import random
 
+import utils
 import bot
 import scheduling
 
+logger = utils.createLogger("main")
+
 if __name__ == "__main__":
+    logger.info("starting water cooler bot...")
     shouldScheduleToday, delay_seconds = scheduling.getTodaySchedule()
     if shouldScheduleToday:
         time.sleep(delay_seconds)
